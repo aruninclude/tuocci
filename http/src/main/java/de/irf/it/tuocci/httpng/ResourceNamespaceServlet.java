cc1241ce13f4be6d1af5f73b33779a276bb4ac48
Alexander Papaspyrou <alexander@papaspyrou.name>
2012-06-14 22:50:38 +0200

    First step towards pure Servlet 2.4-based implementation.

---
+++ http/src/main/java/de/irf/it/tuocci/httpng/ResourceNamespaceServlet.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of tuOCCI.
+ *
+ *     tuOCCI is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as
+ *     published by the Free Software Foundation, either version 3 of
+ *     the License, or (at your option) any later version.
+ *
+ *     tuOCCI is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with tuOCCI.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// $Id$ //
+
+package de.irf.it.tuocci.httpng;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * TODO: not yet commented.
+ *
+ * @author <a href="mailto:alexander.papaspyrou@tu-dortmund.de>Alexander
+ *         Papaspyrou</a>
+ * @version $Revision$ (as of $Date$)
+ */
+public class ResourceNamespaceServlet
+        extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("tuOcci ResourceNamespaceServlet Executed on Path " + request.getRequestURI());
+        out.flush();
+        out.close();
+    }
+}
