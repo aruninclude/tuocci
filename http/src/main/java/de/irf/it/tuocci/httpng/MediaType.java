1d61b149a8a253721096b9f4a43f62954521beec
Alexander Papaspyrou <alexander@papaspyrou.name>
2012-06-15 10:43:27 +0200

    Added media type handler for parsing; necessitating HTTP "Accept" precedence handling.

---
+++ http/src/main/java/de/irf/it/tuocci/httpng/MediaType.java
@@ -0,0 +1,11 @@
+package de.irf.it.tuocci.httpng;
+
+/**
+ * TODO: not yet commented.
+ *
+ * @author <a href="mailto:alexander.papaspyrou@tu-dortmund.de>Alexander
+ *         Papaspyrou</a>
+ * @version $Revision$ (as of $Date$)
+ */
+public enum MediaType {
+}
