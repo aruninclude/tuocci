ff309e446757e2674cd8820c07995998d8213f45
Alexander Papaspyrou <alexander@papaspyrou.name>
2012-07-26 12:13:09 +0200

    Removed header cluttering.

--- model/src/main/java/de/irf/it/tuocci/model/representation/Category.java
+++ model/src/main/java/de/irf/it/tuocci/model/representation/Category.java
@@ -15,40 +15,6 @@
  *     License along with tuOCCI.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of tuOCCI.
- *
- *     tuOCCI is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as
- *     published by the Free Software Foundation, either version 3 of
- *     the License, or (at your option) any later version.
- *
- *     tuOCCI is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with tuOCCI.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * This file is part of tuOCCI.
- *
- *     tuOCCI is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as
- *     published by the Free Software Foundation, either version 3 of
- *     the License, or (at your option) any later version.
- *
- *     tuOCCI is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with tuOCCI.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package de.irf.it.tuocci.model.representation;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
